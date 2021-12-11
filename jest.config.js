992692180facbce38ff71797c3ee75b7a1fbe758
crishellco <cmitchell@crishell.co>
2021-12-10 20:47:33 -0500

    test: exclude index files from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**'],
+  collectCoverageFrom: ['src/**/{!(index**),}.js'],
   coverageReporters: ['json-summary', 'text', 'lcov'],
   preset: '@vue/cli-plugin-unit-jest',
   testMatch: ['**/?(*.)+(spec).[jt]s?(x)'],
